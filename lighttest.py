b05f52ea808b7fcc257a45d318255783e4739f5e
Dan Gartner <dgartner@gmail.com>
2015-05-02 04:53:02 +0000

    Added the speaking thermometer

--- lighttest.py
+++ lighttest.py
@@ -1,18 +1,10 @@
 import RPi.GPIO as GPIO
-import time, os, subprocess
-
+import time
 GPIO.setmode(GPIO.BCM)
-
-pir_pin = 12
-
-GPIO.setup(pir_pin, GPIO.IN)
 GPIO.setup(16, GPIO.OUT)
 GPIO.setup(17, GPIO.OUT)
 GPIO.setup(18, GPIO.OUT)
 GPIO.setup(19, GPIO.OUT)
-
-def say_something(something):
-	subprocess.call(["./button-to-speech/speech.sh", something])
 
 def blink(numTimes, duration):
     for x in range(1, numTimes):        
@@ -36,9 +28,10 @@ def hideLights():
 def lightShow():
     print('Starting the light show')
     for x in range(0, 2):
-        blink(1, 1.25)
+        blink(2, 1.5)
+        blink(2, 1.25)
         blink(2, 1)
-        blink(2, 0.5)
+        blink(3, 0.5)
         blink(20, 0.25)
         blink(40, 0.1)
         blink(1, 3)
@@ -49,18 +42,11 @@ def lightShow():
         blink(5, 0.25)
         blink(20, 0.1)
     print('Light show is complete.')
+        
+        
 
-def motion_handler(pin):
-        print("Motion Detected!")
-        say_something("Hi there Veronica. I heard your favorite color is pink. I made some pretty lights for you.")
-        lightShow()
+lightShow()
 
-hideLights()
-print("Awaiting motion...")
 
-try:
-        GPIO.add_event_detect(pir_pin, GPIO.RISING, callback=motion_handler)
-        while True:
-                time.sleep(5.00)
-except KeyboardInterrupt:
-    GPIO.cleanup()
+
+GPIO.cleanup()
---
+++ temp.py
@@ -0,0 +1,53 @@
+import os, subprocess, glob, time
+import RPi.GPIO as GPIO
+
+pin = 18
+GPIO.setmode(GPIO.BCM)
+GPIO.setup(pin, GPIO.IN, pull_up_down=GPIO.PUD_UP)
+
+os.system('modprobe w1-gpio')
+os.system('modprobe w1-therm')
+
+base_dir = '/sys/bus/w1/devices/'
+device_folder = glob.glob(base_dir + '28*')[0]
+device_file = device_folder + '/w1_slave'
+
+def read_temp_raw():
+    f = open(device_file, 'r')
+    lines = f.readlines()
+    f.close()
+    return lines
+
+def read_temp():
+    lines = read_temp_raw()
+    while lines[0].strip()[-3:] != 'YES':
+          time.sleep(0.2)
+          lines = read_temp_raw()
+    equals_pos = lines[1].find('t=')
+    if equals_pos != -1:
+          temp_string = lines[1][equals_pos+2:]
+          temp_c = float(temp_string) / 1000.0
+          temp_f = temp_c * 9.0 / 5.0 + 32
+          return temp_c, temp_f
+
+def say_temp():
+    temp_c, temp_f = read_temp()
+    msg1 = 'The temp in Celsius is ' + str(temp_c) + ' degrees.'
+    print msg1
+    say_something(msg1)
+    msg2 = 'The temp in farenheight is '+ str(temp_f)  + ' degrees.'
+    print msg2
+    say_something(msg2)
+
+def say_something(something):
+	subprocess.call(["./button-to-speech/speech.sh", something])
+
+
+print('Waiting for inpput')
+try:  
+    while True:
+            if GPIO.input(pin)== False:
+                    say_temp()
+            time.sleep(0.2)
+finally:
+    GPIO.cleanup()
